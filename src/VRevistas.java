8366c04ebd326e7f693e2f5431ca186afc3a404c
MateMar04 <mateo.marchisone@gmail.com>
2021-11-13 18:49:10 -0300

    data load on Revista window

--- src/VRevistas.java
+++ src/VRevistas.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class VRevistas extends javax.swing.JFrame {
     private JComboBox cb_revistas;
@@ -13,5 +17,26 @@ public class VRevistas extends javax.swing.JFrame {
         add(panel1);
         setSize(400, 300);
 
+        ArrayList<Revista> revistas = biblioteca.getRevistas();
+        Vector comboBoxItems = new Vector();
+        for (Revista revista : revistas) {
+            comboBoxItems.add(revista.getTitulo());
+        }
+        cb_revistas.setModel(new DefaultComboBoxModel(comboBoxItems));
+        cb_revistas.setSelectedIndex(-1);
+
+
+        cb_revistas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (cb_revistas.getSelectedIndex() != -1) {
+                    Revista revista = revistas.get(cb_revistas.getSelectedIndex());
+                    lb_titulo.setText(revista.getTitulo());
+                    lb_autor.setText(revista.getAutor().getNombre());
+                    lb_editorial.setText(revista.getEditorial().getNombre());
+                    lb_categoria.setText(revista.getCategoria().getNombre());
+                }
+            }
+        });
     }
 }
